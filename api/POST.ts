807afb665f09f97e9820c36db650c3097fe2bf3d
Steven Klier <saklier@mtu.edu>
2021-07-22 11:24:21 -0500

    created api route folders

---
+++ api/POST.ts
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import AJAX from '../client/src/config';
+
+const POST = {
+  overview: {
+
+  },
+  reviews: {
+
+  },
+  questions: {
+
+  },
+  relatedItems: {
+
+  }
+}
+
+export default POST;
--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import axios from 'axios';
-import AJAX from '../config';
+import React, { useEffect } from 'react';
+import GET from '../../../api/GET';
+// import axios from 'axios';
+// import AJAX from '../config';
 
 import Overview from './Overview';
 import Questions from './Questions';
 import Ratings from './Ratings';
 import RelatedItems from './RelatedItems';
+
+let getOverview = GET.overview.getOverview;
 
 // AJAX Example
 // function ajax() {
@@ -17,6 +20,11 @@ import RelatedItems from './RelatedItems';
 // }
 
 const App = (props: any) => {
+
+  useEffect(() => {
+    getOverview();
+  }, [])
+
   return (
     <div>
       <h1>App</h1>
