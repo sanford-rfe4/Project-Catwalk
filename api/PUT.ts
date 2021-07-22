b17523b05b7e84439ca1dbf201720d4cb9c55632
John Langford <johnrayburnlangford@gmail.com>
2021-07-22 15:26:46 -0500

    added api put requests

--- api/PUT.ts
+++ api/PUT.ts
@@ -1,19 +1,111 @@
-import axios from 'axios';
-import AJAX from '../client/src/config';
+import axios from "axios";
+import AJAX from "../client/src/config";
+
+const header = {
+  Authorization: `${AJAX.API_KEY}`,
+};
 
 const PUT = {
-  overview: {
-
-  },
   reviews: {
-
+    helpful: function (review_id: number) {
+      axios
+        .put(
+          AJAX.URL + `/reviews/${review_id}/helpful`,
+          {},
+          { headers: header }
+        )
+        .then((response) => {
+          console.log("howdy friends");
+        })
+        .catch((err) => {
+          console.log("bye friends");
+        });
+    },
+    report: function (review_id: number) {
+      axios
+        .put(
+          AJAX.URL + `/reviews/${review_id}/report`,
+          {},
+          {
+            headers: header,
+          }
+        )
+        .then((response) => {
+          console.log("howdy friends");
+        })
+        .catch((err) => {
+          console.log("bye friends");
+        });
+    },
   },
   questions: {
-
+    helpful: function (question_id: number) {
+      axios
+        .put(
+          AJAX.URL + `/qa/questions/${question_id}/helpful`,
+          {},
+          {
+            headers: header,
+          }
+        )
+        .then((response) => {
+          console.log("howdy friends");
+        })
+        .catch((err) => {
+          console.log("bye friends");
+        });
+    },
+    report: function (question_id: number) {
+      axios
+        .put(
+          AJAX.URL + `/qa/questions/${question_id}/report`,
+          {},
+          {
+            headers: header,
+          }
+        )
+        .then((response) => {
+          console.log("howdy friends");
+        })
+        .catch((err) => {
+          console.log("bye friends");
+        });
+    },
   },
-  relatedItems: {
-
-  }
-}
+  answers: {
+    helpful: function (answer_id: number) {
+      axios
+        .put(
+          AJAX.URL + `/qa/answers/${answer_id}/helpful`,
+          {},
+          {
+            headers: header,
+          }
+        )
+        .then((response) => {
+          console.log("howdy friends");
+        })
+        .catch((err) => {
+          console.log("bye friends");
+        });
+    },
+    report: function (answer_id: number) {
+      axios.put(
+        AJAX.URL + `/qa/answers/${answer_id}/report`,
+        {},
+        {
+          headers: header,
+        }
+      )
+      .then((response) => {
+        console.log('howdy friends');
+      })
+      .catch((err) => {
+        console.log('bye friends');
+      })
+    },
+  },
+};
 
 export default PUT;
+
