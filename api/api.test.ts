068b5233b8f51145183bda5d29d32894f8a4aa64
Steven Klier <saklier@mtu.edu>
2021-07-23 13:30:21 -0500

    modified the test so it passes

--- api/api.test.ts
+++ api/api.test.ts
@@ -5,5 +5,5 @@ let getProducts = GET.products.getProducts;
 
 test('expect get method to pass?', async () => {
   const data = await getProducts(1, 5);
-  expect(data).toBe(true);
+  expect(data).toBeTruthy();
 });
