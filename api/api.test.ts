6c61190a85805d1afe06784746d7d705cfbb387d
Steven Klier <saklier@mtu.edu>
2021-07-23 14:06:14 -0500

    spiced up the test file

--- api/api.test.ts
+++ api/api.test.ts
@@ -1,9 +1,25 @@
+// DOCS: https://jestjs.io/docs/expect
 import GET from './GET';
-import jest from 'jest';
 
-let getProducts = GET.products.getProducts;
+describe('GET methods', () => {
 
-test('expect get method to pass?', async () => {
-  const data = await getProducts(1, 5);
-  expect(data).toBeTruthy();
+  it('expects getProducts method to return an array of five objects', async () => {
+    const data = await GET.products.getProducts(1, 5);
+    expect(data.length).toBe(5);
+    for (let i = 0; i < data.length; i++) {
+      expect(typeof data[i]).toBe('object');
+      expect(data[i].campus).toBe('hr-rfe');
+    }
+  });
+
 });
+
+describe('POST methods', () => {
+
+});
+
+describe('PUT methods', () => {
+
+});
+
+
