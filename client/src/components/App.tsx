a476e9a9623a469d94e036bc98c3ce573c30e15d
John Langford <johnrayburnlangford@gmail.com>
2021-07-27 15:26:28 -0500

    merge conflict for some reason

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -6,7 +6,6 @@ import Questions from './Questions';
 import Ratings from './Ratings';
 import RelatedItems from './RelatedItems';
 
-<<<<<<< HEAD
 //let getOverview = GET.overview.getOverview;
 
 const App = (props: any) => {
@@ -14,14 +13,6 @@ const App = (props: any) => {
   // useEffect(() => {
   //   getOverview();
   // }, [])
-=======
-const App = (props: any) => {
-
-  // this useEffect hook is the same as componentDidMount
-  useEffect(() => {
-    console.log('componentDidMount');
-  }, [])
->>>>>>> de2c3308e92ba3e75114c47acc1cf3abf6fdc6b1
 
   return (
     <div>
